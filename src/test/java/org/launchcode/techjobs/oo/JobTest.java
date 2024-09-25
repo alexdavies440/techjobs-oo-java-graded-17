bc2f317c58d8c259eafd7ea3143be1e0fd3d8e66
alexdavies440 <alexdavies440@gmail.com>
2024-09-25 16:18:10 -0500

    Task 4 constructor tests done

--- src/test/java/org/launchcode/techjobs/oo/JobTest.java
+++ src/test/java/org/launchcode/techjobs/oo/JobTest.java
@@ -1,5 +1,36 @@
 package org.launchcode.techjobs.oo;
 
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class JobTest {
     //TODO: Create your unit tests here
+
+    @Test
+    public void testSettingJobId() {
+        Job job_one = new Job();
+        Job job_two = new Job();
+        assertNotEquals(job_one.getId(), job_two.getId());
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields() {
+        Job test_job = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
+        assertTrue(test_job.getName() instanceof String);
+        assertEquals("Product tester", test_job.getName());
+
+        assertTrue(test_job.getEmployer() instanceof Employer);
+        assertEquals("ACME", test_job.getEmployer().getValue());
+
+        assertTrue(test_job.getLocation() instanceof Location);
+        assertEquals("Desert", test_job.getLocation().getValue());
+
+        assertTrue(test_job.getPositionType() instanceof PositionType);
+        assertEquals("Quality control", test_job.getPositionType().getValue());
+
+        assertTrue(test_job.getCoreCompetency() instanceof CoreCompetency);
+        assertEquals("Persistence", test_job.getCoreCompetency().getValue());
+    }
 }
