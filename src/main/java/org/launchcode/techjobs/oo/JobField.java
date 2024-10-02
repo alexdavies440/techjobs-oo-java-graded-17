b17d3a32a403ab4fc85e2712a373e8459181347f
alexdavies440 <alexdavies440@gmail.com>
2024-10-01 23:25:31 -0500

    Task 6 checkpoint

--- src/main/java/org/launchcode/techjobs/oo/JobField.java
+++ src/main/java/org/launchcode/techjobs/oo/JobField.java
@@ -1,5 +1,17 @@
 package org.launchcode.techjobs.oo;
 
 public abstract class JobField {
+    private int id;
+    private static int nextId = 1;
+    private String value;
 
+    public JobField() {
+        this.id = nextId;
+        nextId++;
+    }
+
+    public JobField(String value) {
+        this();
+        this.value = value;
+    }
 }
