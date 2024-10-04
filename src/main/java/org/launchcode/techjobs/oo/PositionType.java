b390fa122840b460228fb74200a029608a230628
alexdavies440 <alexdavies440@gmail.com>
2024-10-03 20:57:27 -0500

    Working on refactoring

--- src/main/java/org/launchcode/techjobs/oo/PositionType.java
+++ src/main/java/org/launchcode/techjobs/oo/PositionType.java
@@ -14,45 +14,44 @@ public class PositionType extends JobField {
     }
 
     public PositionType(String value) {
-        this();
-        this.value = value;
+        super(value);
     }
-
-    // TODO: Add a custom toString() method that returns the data stored in 'value'.
-
-    @Override
-    public String toString() {
-        return value;
-    }
-    // TODO: Add custom equals and hashCode methods. Consider two PositionType objects "equal" when
-    //  their id fields match.
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        PositionType that = (PositionType) o;
-        return id == that.id;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(id);
-    }
-
-
-    // Getters and Setters:
-
-    public int getId() {
-        return id;
-    }
-
-    public String getValue() {
-        return value;
-    }
-
-    public void setValue(String value) {
-        this.value = value;
-    }
+//
+//    // TODO: Add a custom toString() method that returns the data stored in 'value'.
+//
+//    @Override
+//    public String toString() {
+//        return value;
+//    }
+//    // TODO: Add custom equals and hashCode methods. Consider two PositionType objects "equal" when
+//    //  their id fields match.
+//
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (o == null || getClass() != o.getClass()) return false;
+//        PositionType that = (PositionType) o;
+//        return id == that.id;
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        return Objects.hashCode(id);
+//    }
+//
+//
+//    // Getters and Setters:
+//
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public String getValue() {
+//        return value;
+//    }
+//
+//    public void setValue(String value) {
+//        this.value = value;
+//    }
 
 }
